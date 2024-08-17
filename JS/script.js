40e896d74b0d12e4f432d75e18f3e449138e5b80
yuta asakura <yuta.asakura.se@gmail.com>
2024-08-18 08:06:46 +0900

    Integrated play-pause button functionality with js

--- JS/script.js
+++ JS/script.js
@@ -29,3 +29,24 @@ filterLink.addEventListener(('click'), (e) => {
   })
 })
 });
+
+// Video
+const videoContainer = document.querySelector('.video-container');
+const mainVideo = document.querySelector('video');
+const playPauseBtn = document.querySelector('.play-pause i');
+
+playPauseBtn.addEventListener('click', () => {
+  if(mainVideo.paused) {
+    mainVideo.play()
+  } else {
+    mainVideo.pause()
+  }
+});
+
+mainVideo.addEventListener('play', () => {
+  playPauseBtn.classList.replace('fa-play', 'fa-pause')
+});
+
+mainVideo.addEventListener('pause', () => {
+  playPauseBtn.classList.replace('fa-pause', 'fa-play')
+});
